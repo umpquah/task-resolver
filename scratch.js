54e7710577bdf21f6d1f725f06725922f2141492
Colin Schatz <schatz@amyris.com>
2024-02-21 18:41:28 -0800

    Add scratchwork

--- scratch.js
+++ scratch.js
@@ -6,32 +6,35 @@ class ConfigError extends Error {
     }
 }
 
-class Variable {
-    constructor(spec) {
+class AbstractVariable {
+    constructor(name, spec) {
+        if(this.constructor == AbstractVariable) {
+            throw new Error("AbstractVariable cannot be instantiated");
+        };
+        this.name = name;
         this.spec = spec;
-        this.refresh();
+        this.refresh(); // run initial randomization as needed
     }
+
     refresh() {
         // no op
     }
-}
 
-class StaticVariable extends Variable {
-    get value() {
-        return this.spec;
-    }
-}
-
-class RandomVariable extends Variable {
     get value() {
         return this._value;
     }
-    refresh() {
-        this._value = null;
-    }
 }
 
-class RangeVariable extends RandomVariable {
+class StaticVariable extends AbstractVariable {
+    constructor(name, spec) {
+        super(name, spec);
+     
+        this._value = spec;
+    }
+
+}
+
+class RangeVariable extends AbstractVariable {
     refresh() {
         // TODO: Check spec has valid values
         const [min, max] = this.spec;
@@ -39,7 +42,7 @@ class RangeVariable extends RandomVariable {
     }
 }
 
-class BoolVariable extends RandomVariable {
+class BoolVariable extends AbstractVariable {
     refresh() {
         // TODO: Check spec has valid value
         const prob = this.spec;
@@ -47,7 +50,7 @@ class BoolVariable extends RandomVariable {
     }
 }
 
-class SelectVariable extends RandomVariable {
+class SelectVariable extends AbstractVariable {
     refresh() {
         // TODO: Check spec has valid value
         const options = this.spec;
@@ -56,14 +59,17 @@ class SelectVariable extends RandomVariable {
     }
 }
 
-class DerivedVariable extends Variable {
-    constructor(spec, derivedFromVariables) {
-        this.spec = spec;
-        this._derivedFrom = derivedFromVariables;
-        this.refresh();
+class DerivedVariable extends AbstractVariable {
+    constructor(name, spec, derivedFromVariables) {
+        super(name, spec);
+        const variableNames = derivedFromVariables.map((v) => (v.name));
+        const fnBody = `return ${spec};`;
+        const fn = new Function(...variableNames, fnBody);
+        this.value = fn();
     }
+
     refresh() {
-        
+        // N/A
     }
 }
 
@@ -73,19 +79,18 @@ const PARAM_TYPES = {
     "bool": BoolVariable,
     "select": SelectVariable,
 }
-const STAGE_PROPS = ["initialStage", "label", "params", "result", "actions"];
-const ACTION_TYPES = ["report", "next"];
-
-const RESERVED_NAMES = Object.keys(PARAM_TYPES).concat(STAGE_PROPS).concat(ACTION_TYPES);
+const STAGE_PROPS = ["initialStage", "label", "parameters", "results", "resolution"];
+const RESOLUTION_PROPS = ["announce", "action", "wait", "next"];
+const RESERVED_NAMES = Object.keys(PARAM_TYPES).concat(STAGE_PROPS).concat(RESOLUTION_PROPS);
 
 class Stage {
-    constructor({ initialStage, label, params, result, actions }) {
+    constructor({ initialStage, label, params, results, resolution }) {
         this.initialStage = initialStage;
         this.label = label;
         this._variables = {};
         this._setup_params(params);
-        this._setup_actions(actions);
-        this._result = result;
+        // this._setup_actions(actions);
+        // this._result = result;
         this.refresh();
     }
 
@@ -101,27 +106,21 @@ class Stage {
                 if (RESERVED_NAMES.includes(name)) {
                     throw new ConfigError(`Param names cannot include reserved word '${name}'`);
                 }
-                this._variables[name] = new variable_cls(spec);
+                this._variables[name] = new variable_cls(name, spec);
             }
         }
     }
 
-    _setup_actions(actions) {
-        this._actions = {};
-        for (const type in actions) {
-            if (!ACTION_TYPES.includes(type)) {
-                throw new ConfigError(`Invalid action type '${type}'`)
-            }
-        }
-    }
+    // _setup_actions(actions) {
+    //     this._actions = {};
+    //     for (const type in actions) {
+    //         if (!ACTION_TYPES.includes(type)) {
+    //             throw new ConfigError(`Invalid action type '${type}'`)
+    //         }
+    //     }
+    // }
 
-    _build_fn(variables, expression) {
-        const consts = Object.entries(variables).map(([key, param]) => (
-            `const ${key} = ${JSON.stringify(param.value)};`
-        )).join("\n")
-        const body = `${consts}\n\nreturn ${expression};`;
-        return new Function(body);
-    }
+
 
     refresh() {
         Object.values(this._variables).forEach(p => { p.refresh() });
@@ -146,14 +145,14 @@ const stages = {
     stageA: {
         initialStage: true, // Error if set for more than one
         label: "Stage A",
-        params: {
+        parameters: {
             static: {
                 baseAmount: 3,
                 units: ["step", "steps"],
                 directions: ["left", "right"],
             },
             range: {
-                dist: [1, 11],
+                steps: [1, 11],
             },
             select: {
                 color: ["red", "blue", "green", "purple"],
@@ -162,9 +161,11 @@ const stages = {
                 goRight: 0.8,
             },
         },
-        result: "baseAmount * dist",
-        actions: {
-            report: "`Go ${result} ${color} steps ${goRight ? 'right' : 'left'}`",
+        results: {
+            distance: "baseAmount * steps",
+        },
+        resolution: {
+            report: "`Go ${distance} ${color} steps ${goRight ? 'right' : 'left'}`",
             next: "goRight ? 'stageB' : 'stageC'",
         }
     },
@@ -176,11 +177,23 @@ const stages = {
     }
 };
 
-const stage = new Stage(stages.stageA)
+// const stage = new Stage(stages.stageA)
+
+// for (let i = 0; i < 10; i++) {
+//     // console.dir(stage.current_params);
+//     console.log(stage.result);
+//     // console.log();
+//     stage.refresh();
+// }
+
+// const v = new AbstractVariable("age", "blah");
+let dozens = new RangeVariable("dozens", [2, 7]);
+let base = new StaticVariable("base", 12);
+
+let eggs = new DerivedVariable("eggs", "base * dozens", [dozens, base])
 
 for (let i = 0; i < 10; i++) {
-    // console.dir(stage.current_params);
-    console.log(stage.result);
-    // console.log();
-    stage.refresh();
+    console.log(`${dozens.value} ${base.value} --> ${eggs.value}`);
+    dozens.refresh();
+    base.refresh();
 }
